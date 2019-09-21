4206ca4f03a710ddc741d8d515dbd49b529336d5
Eric Ren <546296+rencire@users.noreply.github.com>
2019-09-20 17:10:14 -0700

    feat: return template, style, and script

--- WCLoader.js
+++ WCLoader.js
@@ -5,7 +5,19 @@ const fetchAndParse = (url) =>
     })
     .then( html => {
       const parser = new DOMParser();
-      return parser.parseFromString(html, 'text/html');
+      const document = parser.parseFromString(html, 'text/html');
+      // Parser will put <template>, <style>, and <script> in <head>
+      // https://html.spec.whatwg.org/multipage/parsing.html#tree-construction
+      const head = document.head;
+      const template = head.querySelector('template');
+      const style = head.querySelector('style');
+      const script = head.querySelector('script');
+     
+      return {
+        template,
+        style,
+        script 
+      }
     });
 
 const loadComponent = (url) => fetchAndParse(url);
